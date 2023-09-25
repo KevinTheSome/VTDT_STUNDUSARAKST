98ab3a307b1ce29dae01074d08e1f6308d988d8a
KevinTheSome <KevinsKanalis@gmail.com>
2023-09-25 21:13:17 +0300

    added the base

---
+++ firstviteproject/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="app">
+      <p>Letsee if it works</p>
+    </div>
+    </>
+  )
+}
+
+export default App
